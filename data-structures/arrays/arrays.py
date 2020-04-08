703ed5119a04fa0ee7f38138d283dddb7818fec2
angel-torres <alas4224@gmail.com>
2020-04-08 17:23:58 -0500

    added a pop method

--- data-structures/arrays/arrays.py
+++ data-structures/arrays/arrays.py
@@ -5,16 +5,32 @@ class Array:
         self.size = 0
     
     def __str__(self):
-        return f"{self.storage}"
+        return f"{self.storage} - Size: {self.size}"
 
     def insert(self, item):
+        if self.size == self.capacity:
+            print("Array is full!")
+            return
+
         for i in range(len(self.storage) - 1, 0, -1):
             self.storage[i] = self.storage[i - 1]
-            print(self.storage[i], self.storage[i- 1])
-            print(i, i- 1)
 
         self.storage[0] = item
         self.size += 1
+    
+    def append(self, item):
+        if self.size == self.capacity:
+            print("Array is full!")
+            return
+
+        self.storage[self.size] = item
+        self.size += 1
+    
+    def pop(self):
+        last_item = self.storage[self.size - 1]
+        self.storage[self.size - 1] = None
+        self.size -= 1
+        return last_item
 
 newArray = Array(8)
 print(newArray)
@@ -24,7 +40,12 @@ newArray.insert(5)
 newArray.insert(6)
 newArray.insert(7)
 newArray.insert(8)
-newArray.insert(9)
-newArray.insert(10)
-newArray.insert(11)
+newArray.append(2)
+newArray.append(1)
+print(newArray.pop())
+print(newArray.pop())
+print(newArray.pop())
+newArray.append(9)
+# newArray.insert(10)
+# newArray.insert(11)
 print(newArray)

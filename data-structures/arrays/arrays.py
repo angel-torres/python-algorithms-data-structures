a7d818300ced937c904230e593cf7697db88dec1
angel-torres <alas4224@gmail.com>
2020-04-08 18:38:56 -0500

    added remove method

--- data-structures/arrays/arrays.py
+++ data-structures/arrays/arrays.py
@@ -31,6 +31,17 @@ class Array:
         self.storage[self.size - 1] = None
         self.size -= 1
         return last_item
+    
+    def remove(self, item):
+        for i in range(self.size):
+            if self.storage[i] == item:
+                for i in range(i, self.size - 1, 1):
+                    self.storage[i] = self.storage[i + 1]
+                    print(self.storage[i])
+                self.storage[self.size - 1] = None
+                self.size -= 1
+
+
 
 newArray = Array(8)
 print(newArray)
@@ -42,10 +53,14 @@ newArray.insert(7)
 newArray.insert(8)
 newArray.append(2)
 newArray.append(1)
-print(newArray.pop())
-print(newArray.pop())
-print(newArray.pop())
-newArray.append(9)
+newArray.remove(6)
+newArray.remove(7)
+newArray.remove(5)
+newArray.append(1)
+# print(newArray.pop())
+# print(newArray.pop())
+# print(newArray.pop())
+# newArray.append(9)
 # newArray.insert(10)
 # newArray.insert(11)
 print(newArray)

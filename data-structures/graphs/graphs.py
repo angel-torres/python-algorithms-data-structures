3359315660aa28bc5a4a61cf87875de50651cf87
angel-torres <alas4224@gmail.com>
2020-04-27 19:32:26 -0500

    implemented bft

--- data-structures/graphs/graphs.py
+++ data-structures/graphs/graphs.py
@@ -1,3 +1,5 @@
+from queue import Queue
+
 class Graph:
     def __init__(self):
         self.vertices = {}
@@ -12,30 +14,91 @@ class Graph:
     def get_neighbors(self, vertex):
         return self.vertices[vertex]
 
-    def dft(self, vertex):
-        print(vertex)
+    def bft(self, vertex):
+        visited = set()
+        path = []
+        plan_to_visit = Queue()
 
-new_graph = Graph()
-new_graph.add_vertex(1)
-new_graph.add_vertex(2)
-new_graph.add_vertex(3)
-new_graph.add_vertex(4)
-new_graph.add_vertex(5)
-new_graph.add_vertex(6)
-new_graph.add_vertex(7)
-new_graph.add_edge(1, 2)
-new_graph.add_edge(2, 1)
-new_graph.add_edge(1, 3)
-new_graph.add_edge(3, 1)
-new_graph.add_edge(1, 4)
-new_graph.add_edge(4, 1)
-new_graph.add_edge(2, 5)
-new_graph.add_edge(5, 2)
-new_graph.add_edge(2, 6)
-new_graph.add_edge(6, 2)
-new_graph.add_edge(2, 7)
-new_graph.add_edge(7, 2)
+        plan_to_visit.queue(vertex)
 
-print(new_graph.vertices)
-print(new_graph.get_neighbors(1))
-print(new_graph.dft(1))
+        while plan_to_visit.size > 0:
+            current_vertex = plan_to_visit.dequeue()
+            path.append(current_vertex)
+            if current_vertex not in visited:
+                visited.add(current_vertex)
+
+            neighbors = self.get_neighbors(current_vertex)
+            for neighbor in neighbors:
+                if neighbor not in visited:
+                    plan_to_visit.queue(neighbor)
+        
+        return path
+
+
+if __name__ == '__main__':
+    graph = Graph()  # Instantiate your graph
+    # https://github.com/LambdaSchool/Graphs/blob/master/objectives/breadth-first-search/img/bfs-visit-order.png
+    graph.add_vertex(1)
+    graph.add_vertex(2)
+    graph.add_vertex(3)
+    graph.add_vertex(4)
+    graph.add_vertex(5)
+    graph.add_vertex(6)
+    graph.add_vertex(7)
+    graph.add_edge(5, 3)
+    graph.add_edge(6, 3)
+    graph.add_edge(7, 1)
+    graph.add_edge(4, 7)
+    graph.add_edge(1, 2)
+    graph.add_edge(7, 6)
+    graph.add_edge(2, 4)
+    graph.add_edge(3, 5)
+    graph.add_edge(2, 3)
+    graph.add_edge(4, 6)
+
+    '''
+    Should print:
+        {1: {2}, 2: {3, 4}, 3: {5}, 4: {6, 7}, 5: {3}, 6: {3}, 7: {1, 6}}
+    '''
+    print(graph.vertices)
+
+    '''
+    Valid BFT paths:
+        1, 2, 3, 4, 5, 6, 7
+        1, 2, 3, 4, 5, 7, 6
+        1, 2, 3, 4, 6, 7, 5
+        1, 2, 3, 4, 6, 5, 7
+        1, 2, 3, 4, 7, 6, 5
+        1, 2, 3, 4, 7, 5, 6
+        1, 2, 4, 3, 5, 6, 7
+        1, 2, 4, 3, 5, 7, 6
+        1, 2, 4, 3, 6, 7, 5
+        1, 2, 4, 3, 6, 5, 7
+        1, 2, 4, 3, 7, 6, 5
+        1, 2, 4, 3, 7, 5, 6
+    '''
+    # graph.bft(1)
+
+    '''
+    Valid DFT paths:
+        1, 2, 3, 5, 4, 6, 7
+        1, 2, 3, 5, 4, 7, 6
+        1, 2, 4, 7, 6, 3, 5
+        1, 2, 4, 6, 3, 5, 7
+    '''
+    print(graph.dft(1))
+    # graph.dft_recursive(1)
+
+    '''
+    Valid BFS path:
+        [1, 2, 4, 6]
+    '''
+    # print(graph.bfs(1, 6))
+
+    '''
+    Valid DFS paths:
+        [1, 2, 4, 6]
+        [1, 2, 4, 7, 6]
+    '''
+    # print(graph.dfs(1, 6))
+    # print(graph.dfs_recursive(1, 6))
---
+++ data-structures/graphs/queue.py
@@ -0,0 +1,22 @@
+class Queue:
+    def __init__(self):
+        self.storage = []
+        self.size = 0
+    
+    def queue(self, value):
+        self.storage.append(value)
+        self.size += 1
+
+    def dequeue(self):
+        self.size -= 1
+        return self.storage.pop(0)
+
+
+new_queu = Queue()
+new_queu.queue(1)
+new_queu.queue(2)
+new_queu.queue(3)
+print(new_queu.storage)
+print(new_queu.dequeue())
+print(new_queu.dequeue())
+print(new_queu.storage)
